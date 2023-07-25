1e2ead2cdbd372ffa9732e9d8ccfd890655e118f
Calladrus2001 <dugarvishesh@gmail.com>
2023-07-25 11:41:48 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Vishesh Dugar on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
