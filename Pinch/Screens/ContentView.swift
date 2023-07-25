7550353915ecf6b37399945c32b1aa225a4919f1
Calladrus2001 <dugarvishesh@gmail.com>
2023-07-25 19:17:35 +0530

    drag gesture

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -11,8 +11,13 @@ struct ContentView: View {
   // MARK: - Property
   @State private var isAnimated: Bool = false
   @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = .zero
+  
   
   // MARK: - Function
+  func resetImageState() {
+    return withAnimation(.spring()) {imageScale = 1; imageOffset = .zero}
+  }
   
   // MARK: - Content
     var body: some View {
@@ -26,16 +31,32 @@ struct ContentView: View {
             .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
             .opacity(isAnimated ? 1 : 0)
             .animation(.linear(duration: 1), value: isAnimated)
+            .offset(x: imageOffset.width, y: imageOffset.height)
             .scaleEffect(imageScale)
           
             //MARK: - 1. Tap Gesture
             .onTapGesture(count: 2) {
               if imageScale == 1 {
-                withAnimation(.spring()) {imageScale = 5}
+                withAnimation(.spring()) {imageScale = 5; imageOffset = .zero}
               } else {
-                withAnimation(.spring()) {imageScale = 1}
+                resetImageState()
               }
             }
+          
+            //MARK: - 2. Drag Gesture
+            .gesture(
+              DragGesture()
+                .onChanged {
+                  value in withAnimation(.linear(duration: 1)) {
+                    imageOffset = value.translation
+                  }
+                }
+                .onEnded {_ in
+                  if (imageScale <= 1) {
+                    resetImageState()
+                  }
+                }
+            )
         }
         .navigationTitle("Pinch & Zoom")
         .navigationBarTitleDisplayMode(.inline)
