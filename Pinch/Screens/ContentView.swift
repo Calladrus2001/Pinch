9be2427417e357f9f7a60eecd36c81f4280b8cc9
Calladrus2001 <dugarvishesh@gmail.com>
2023-07-27 15:28:16 +0530

    Control Image Magnify Panel

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -23,6 +23,9 @@ struct ContentView: View {
     var body: some View {
       NavigationView {
         ZStack {
+          
+          Color.clear
+          
           Image("magazine-front-cover")
             .resizable()
             .aspectRatio(contentMode: .fit)
@@ -63,9 +66,65 @@ struct ContentView: View {
         .onAppear(perform: {
           isAnimated = true
         })
+        
+        //MARK: - Info Panel
+        .overlay(
+          InfoPanelView(scale: imageScale, offset: imageOffset),
+          alignment: .top
+        )
+        
+        //MARK: - Controls
+        .overlay(
+          Group {
+            HStack {
+              // SCALE DOWN
+              Button {
+                withAnimation(.spring()) {
+                  if imageScale > 1 {
+                    imageScale -= 1
+                    
+                    if imageScale <= 1 {
+                      resetImageState()
+                    }
+                  }
+                }
+              } label: {
+                ControlImageView(icon: "minus.magnifyingglass")
+              }
+              
+              // RESET
+              Button {
+                resetImageState()
+              } label: {
+                ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+              }
+              
+              // SCALE UP
+              Button {
+                withAnimation(.spring()) {
+                  if imageScale < 5 {
+                    imageScale += 1
+                    
+                    if imageScale > 5 {
+                      imageScale = 5
+                    }
+                  }
+                }
+              } label: {
+                ControlImageView(icon: "plus.magnifyingglass")
+              }
+              
+            } //: CONTROLS
+            .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+            .background(.ultraThinMaterial)
+            .cornerRadius(12)
+            .opacity(isAnimated ? 1 : 0)
+          }
+            .padding(.bottom, 30)
+          , alignment: .bottom
+        )
       }
       .navigationViewStyle(.stack)
-      
     }
 }
 
